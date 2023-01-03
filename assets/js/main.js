0da26a05fef1d2ef878f8b07cf4d016c4cbb7f5a
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 12:09:12 +0100

    JavaScript_Lev1_7

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+console.log("Test");
+
+let helloWorld = "Hello World"
+console.log("helloWorld");
+
+const pi = 3.14
+console.log(pi);
+
+const burjDubai = "828m"
+console.log(burjDubai);
+
+let fullName = "Jan Schmidt"
+console.log(fullName);
+
+const eifelTower = "324m"
+console.log(eifelTower);
+
+const camelCase = "camelCase"
+console.log(camelCase);
+
+let kursStatus = "true"
+console.log(kursStatus);
